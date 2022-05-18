d1c19431b0025ced7f396c5a22f1014b36a4f0e3
kuroringo90 <ssss9999@gmx.it>
2022-05-18 21:47:34 +0200

    vayu: Initial KOSP bringup 2

---
+++ aosp_vayu.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from vayu device
+$(call inherit-product, device/xiaomi/vayu/device.mk)
+
+# Inherit some common kraken stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+# Boot Animation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := aosp_vayu
+PRODUCT_DEVICE := vayu
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO X3 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
